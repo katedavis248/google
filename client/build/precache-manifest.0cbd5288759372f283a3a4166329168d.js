b1704617271b8880df209ba6b6430bd8bbf75ba5
Kate Davis <katedavis@Kates-MacBook-Pro.local>
2020-02-27 21:37:34 -0600

    yay

---
+++ client/build/precache-manifest.0cbd5288759372f283a3a4166329168d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8971311b98df3b9b9b84",
+    "url": "/static/css/main.f55794d6.chunk.css"
+  },
+  {
+    "revision": "8971311b98df3b9b9b84",
+    "url": "/static/js/main.f724b199.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2583ef62e49bcb86fb41",
+    "url": "/static/css/2.ed30ee0c.chunk.css"
+  },
+  {
+    "revision": "2583ef62e49bcb86fb41",
+    "url": "/static/js/2.0e752d98.chunk.js"
+  },
+  {
+    "revision": "71bd44a5988abe3e11be7e13e1badde4",
+    "url": "/index.html"
+  }
+];
